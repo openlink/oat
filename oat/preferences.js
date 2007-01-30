43fb61c81673a48c83c97e287d6e9b9d744e7f3c
OAT Maintainer <oat.admin@openlinksw.com>
2007-01-30 15:55:38 +0000

    * Updated date field

--- oat/preferences.js
+++ oat/preferences.js
@@ -10,9 +10,9 @@
 OAT.Preferences = {
 	showAjax:1, /* show Ajax window even if not explicitly requested by application? */
 	useCursors:1, /* scrollable cursors */
-	windowTypeOverride:2, /* do not guess window type */
+	windowTypeOverride:0, /* do not guess window type */
 	xsltPath:"/DAV/JS/xslt",
-	version:"11.12.2006",
+	version:"29.1.2006",
 	httpError:1, /* show http errors */
 	allowDefaultResize:1,
 	allowDefaultDrag:1
