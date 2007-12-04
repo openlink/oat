0ddad9c1dc12686d67d91ef54f1b0ef39b8656d0
OAT Maintainer <oat.admin@openlinksw.com>
2007-12-04 14:20:40 +0000

    * Added support for oai protocol * Fixed parsing http://user:pass@host/ * Fixed queying proxy for N3 file; specify output format as N3 in request

--- oat/dereference.js
+++ oat/dereference.js
@@ -14,12 +14,16 @@
 OAT.Dereference = {
 	go:function(url,callback,optObj) {
 		if (url.match(/^http/i)) { /* Virtuoso proxy: */
-			var r = url.match(/^http:\/\/([^@]+@)?(.*)/);
+			var r = url.match(/^http:\/\/([^@\/]+@)?(.*)/);
 			var user = (r[1] ? r[1].substring(0,r[1].length-1) : false);
-			
 			var encoded = encodeURIComponent("http://"+r[2]);
 			encoded = "/proxy?url="+encoded+"&force=rdf";
 			if (user) { encoded += "&login="+encodeURIComponent(user); }
+			if (url.match(/\.n3$/)) { encoded += "&output-format=n3"; }
+		} else if (url.match(/^urn:/i) || url.match(/^doi:/i) || url.match(/^oai:/i)) { /* Virtuoso proxy: */
+			var encoded = encodeURIComponent(url);
+			encoded = "/proxy?url="+encoded+"&force=rdf";
+			if (url.match(/\.n3$/)) { encoded += "&output-format=n3"; }
 		} else {
 			var encoded = url;
 		}
