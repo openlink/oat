c5fc041475cfbeb4c1df4cb9492d07d0fddb27fb
OAT Maintainer <oat.admin@openlinksw.com>
2014-04-29 14:48:35 +0200

    Added browser specific JSON replacement
    * Added JSON parser that ONLY uses the browser supplied version
    * Commented out code that needed enablePrivilege elevation

---
+++ oat/json_ff.js
@@ -0,0 +1,29 @@
+/*
+ *  $Id$
+ *
+ *  This file is part of the OpenLink Software Ajax Toolkit (OAT) project.
+ *
+ *  Copyright (C) 2005-2012 OpenLink Software
+ *
+ *  See LICENSE file for details.
+ *
+ */
+
+// special version create for usign with FF extensions only, 
+// for avoid problem with FF extension verification process
+
+OAT.JSON = {
+
+	deserialize:function(jsonString) {
+               	return JSON.parse(jsonString);
+	},
+			
+	serialize:function(something, c) {
+       		return JSON.stringify(something);
+	}
+
+}
+
+//  Backward compatibility
+OAT.JSON.stringify = OAT.JSON.serialize;
+OAT.JSON.parse     = OAT.JSON.deserialize;
